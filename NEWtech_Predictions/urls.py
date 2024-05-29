93b01d52c906bd0ec5186cc257b20525db2e41cf
Héctor Macías <hmacias54@uabc.edu.mx>
2024-05-29 06:56:12 -0700

    estilos y boostrap añadido

--- NEWtech_Predictions/urls.py
+++ NEWtech_Predictions/urls.py
@@ -20,4 +20,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 """
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('newtech.urls')),
+]
 
